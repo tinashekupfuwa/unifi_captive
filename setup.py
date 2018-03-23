231b62720c6f1121cbd26c378b8e8c21f2caedac
nryzhkov <nryzhkov@vbrr.ru>
2018-03-23 06:27:06 +0000

    Update 'setup.py'

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=['alembic', 'Flask', 'Flask-Login', 'Flask-Migrate', 'Flask-SQLAlchemy', 'Flask-WTF',
-    'Jinja2', 'Mako', 'pyunifi', 'requests', 'SQLAlchemy', 'urllib3', 'Werkzeug', 'WTForms' ],
+    'Jinja2', 'Mako', 'pyunifi', 'requests', 'SQLAlchemy', 'urllib3', 'Werkzeug', 'WTForms', 'smpplib'],
     entry_points={ 'flask.commands': [ 'db=flask_migrate.cli:db' ], },
 )
